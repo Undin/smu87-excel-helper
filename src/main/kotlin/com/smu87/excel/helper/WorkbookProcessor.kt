b40238be04b0c03a33e9416fa8a56e4e63615b93
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-03-09 12:48:00 +0300

    Make rows of main suppliers bold

--- src/main/kotlin/com/smu87/excel/helper/WorkbookProcessor.kt
+++ src/main/kotlin/com/smu87/excel/helper/WorkbookProcessor.kt
@@ -11,12 +11,19 @@ class WorkbookProcessor(private val workbook: XSSFWorkbook) {
     private val outputWorkbook: XSSFWorkbook = XSSFWorkbook()
 
     private val precisionStyle: CellStyle
+    private val boldStyle: CellStyle
+    private val boldPrecisionStyle: CellStyle
 
     init {
-        precisionStyle = outputWorkbook.createCellStyle()
         val dataFormat = outputWorkbook.createDataFormat()
-        val precisionFormat = dataFormat.getFormat("0.00")
-        precisionStyle.dataFormat = precisionFormat
+        val precisionFormatIndex = dataFormat.getFormat("0.00")
+
+        val boldFont = outputWorkbook.createFont()
+        boldFont.bold = true
+
+        precisionStyle = outputWorkbook.createCellStyle(null, precisionFormatIndex)
+        boldStyle = outputWorkbook.createCellStyle(boldFont, null)
+        boldPrecisionStyle = outputWorkbook.createCellStyle(boldFont, precisionFormatIndex)
     }
 
     fun process(): XSSFWorkbook {
@@ -47,6 +54,9 @@ class WorkbookProcessor(private val workbook: XSSFWorkbook) {
             .sortedWith(ROW_COMPARATOR)
             .forEachIndexed { index, row ->
                 val outputRow = outputSheet.createRow(index + 1)
+                if (outputRow.isMainSupplierRow) {
+                    outputRow.rowStyle = boldStyle
+                }
                 processRow(row, outputRow)
             }
     }
@@ -57,15 +67,18 @@ class WorkbookProcessor(private val workbook: XSSFWorkbook) {
         var amount: Double? = null
         var isCountable = false
         for (i in 0 until 7) {
+            // number, amount, price and cost are numbers. otherwise it's string
+            val cellType = if (i in listOf(0, 4, 5, 6)) CellType.NUMERIC else CellType.STRING
+            val outputCell = outputRow.createCell(i, cellType)
+            var setPrecisionStyle = false
+
             when (i) {
                 // number
                 0 -> {
-                    val outputCell = outputRow.createCell(i, CellType.NUMERIC)
                     outputCell.setCellValue(outputRow.rowNum.toDouble())
                 }
                 // unit
                 3 -> {
-                    val outputCell = outputRow.createCell(i, CellType.STRING)
                     val unit = row[i].value
                     val result = UNIT_PATTERN.matchEntire(unit)?.groups
                     val unitValue = if (result != null) {
@@ -86,26 +99,28 @@ class WorkbookProcessor(private val workbook: XSSFWorkbook) {
                     if (isCountable) {
                         amount = amount.roundToInt().toDouble()
                     }
-                    val outputCell = outputRow.createCell(i, CellType.NUMERIC)
                     outputCell.setCellValue(amount)
                 }
                 // price
                 5 -> {
                     price = row[i].value.toDouble() / multiplier
-                    val outputCell = outputRow.createCell(i, CellType.NUMERIC)
                     outputCell.setCellValue(price)
-                    outputCell.cellStyle = precisionStyle
+                    setPrecisionStyle = true
                 }
                 // cost
                 6 -> {
-                    val outputCell = outputRow.createCell(i, CellType.NUMERIC)
                     outputCell.setCellValue(price!! * amount!!)
-                    outputCell.cellStyle = precisionStyle
+                    setPrecisionStyle = true
                 }
                 else -> {
-                    val outputCell = outputRow.createCell(i, CellType.STRING)
                     outputCell.setCellValue(row[i].value)
                 }
+            }
+
+            outputCell.cellStyle = when {
+                row.isMainSupplierRow -> if (setPrecisionStyle) boldPrecisionStyle else boldStyle
+                setPrecisionStyle -> precisionStyle
+                else -> null
             }
         }
     }
@@ -114,13 +129,26 @@ class WorkbookProcessor(private val workbook: XSSFWorkbook) {
         private val SECONDARY_SUPPLIER: Regex = """\d*-\d*""".toRegex()
         private val UNIT_PATTERN: Regex = """(?<count>\d+) (?<unit>.+)""".toRegex()
         private val ROW_COMPARATOR: Comparator<Row> = Comparator.comparing<Row, Boolean> {
-            it.getCell(1).value.matches(SECONDARY_SUPPLIER)
+            !it.isMainSupplierRow
         }.thenBy {
             it.getCell(2).value
         }
 
+        private val Row.isMainSupplierRow: Boolean get() = !getCell(1).value.matches(SECONDARY_SUPPLIER)
+
         private operator fun Row.get(index: Int): Cell? = getCell(index)
 
         private val Cell?.value: String get() = this?.toString().orEmpty().trim()
+
+        private fun XSSFWorkbook.createCellStyle(font: Font?, dataFormatIndex: Short?): CellStyle {
+            val style = createCellStyle()
+            if (font != null) {
+                style.setFont(font)
+            }
+            if (dataFormatIndex != null) {
+                style.dataFormat = dataFormatIndex
+            }
+            return style
+        }
     }
 }
