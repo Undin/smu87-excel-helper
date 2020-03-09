48c58af828aff82118e01b70248e8c677cfe12dc
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-03-09 13:58:48 +0300

    Show dialog on error

--- src/main/kotlin/com/smu87/excel/helper/Main.kt
+++ src/main/kotlin/com/smu87/excel/helper/Main.kt
@@ -1,7 +1,6 @@
 package com.smu87.excel.helper
 
 import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.IOException
@@ -9,17 +8,43 @@ import java.text.SimpleDateFormat
 import java.util.*
 import javax.swing.JFileChooser
 import javax.swing.JFrame
+import javax.swing.JOptionPane
 import javax.swing.UIManager
 import javax.swing.filechooser.FileNameExtensionFilter
 
 object Main {
 
     private val LOG = LogManager.getLogger(Main::class.java)
+    private val DATE_FORMAT: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val inputFile = chooseFile() ?: return
-        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(inputFile))
+        setSystemLookAndFeel()
+
+        val frame = JFrame()
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        val inputFile = chooseFile(frame)
+
+        try {
+            inputFile?.let(this::doWork)
+        } catch (e: Throwable) {
+            LOG.error(e.message, e)
+            inputFile?.let(this::copyFile)
+            showErrorDialog(frame)
+        } finally {
+            frame.dispose()
+        }
+    }
+
+    private fun copyFile(file: File) {
+        val fileName = "${file.nameWithoutExtension} (${DATE_FORMAT.format(Date())}).${file.extension}"
+        try {
+            val dst = File("problemFiles", fileName)
+            file.copyTo(dst)
+        } catch (ignore: Throwable) {}
+    }
+
+    private fun doWork(inputFile: File) {
         LOG.info("File `${inputFile.absolutePath}` is chosen")
 
         val inputWorkbook = XSSFWorkbook(inputFile)
@@ -35,24 +60,20 @@ object Main {
         }
     }
 
-    private fun chooseFile(): File? {
-        setSystemLookAndFeel()
-
-        val frame = JFrame()
-        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    private fun chooseFile(frame: JFrame): File? {
         val chooser = JFileChooser()
         chooser.fileSelectionMode = JFileChooser.FILES_ONLY
         chooser.fileFilter = FileNameExtensionFilter("", "xlsx")
         val result = chooser.showOpenDialog(frame)
-        try {
-            return if (result == JFileChooser.APPROVE_OPTION) {
-                chooser.selectedFile
-            } else {
-                null
-            }
-        } finally {
-            frame.dispose()
+        return if (result == JFileChooser.APPROVE_OPTION) {
+            chooser.selectedFile
+        } else {
+            null
         }
+    }
+
+    private fun showErrorDialog(frame: JFrame) {
+        JOptionPane.showMessageDialog(frame, "Что-то пошло не так", "Ошибка", JOptionPane.ERROR_MESSAGE)
     }
 
     private fun setSystemLookAndFeel() {
@@ -60,22 +81,6 @@ object Main {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
         } catch (e: Exception) {
             LOG.warn(e.message, e)
-        }
-    }
-
-    private class ExceptionHandler(private val file: File) : Thread.UncaughtExceptionHandler {
-        override fun uncaughtException(t: Thread, e: Throwable) {
-            LOG.error(e.message, e)
-            val fileName = "${file.nameWithoutExtension} (${DATE_FORMAT.format(Date())}).${file.extension}"
-            try {
-                val dst = File("problemFiles", fileName)
-                file.copyTo(dst)
-            } catch (ignore: Exception) {}
-        }
-
-        companion object {
-            private val LOG: Logger = LogManager.getLogger(ExceptionHandler::class.java)
-            private val DATE_FORMAT: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         }
     }
 }
